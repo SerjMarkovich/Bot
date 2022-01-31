03e36cd0f9ff80159d2c84b75d10c2b63fe4e430
Nikita Voloshin <n.voloshin@just-ai.com>
2022-01-31 21:13:39 +0300

    Fix api urls endings

--- core/src/main/kotlin/com/justai/jaicf/helpers/http/URL.kt
+++ core/src/main/kotlin/com/justai/jaicf/helpers/http/URL.kt
@@ -1,3 +1,5 @@
 package com.justai.jaicf.helpers.http
 
 fun String.toUrl() = this.replace("(?<=[^:\\s])(/+/)".toRegex(), "/")
+
+fun String.withTrailingSlash(with: Boolean = true) = removeSuffix("/") + if (with) "/" else ""
